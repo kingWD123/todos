b4c5439ad647c422de128ea94786e607285cc317
MELAS-exe <smohamedamine@ept.sn>
2025-07-09 09:53:41 +0000

    pushing the entire project

---
+++ src/test/java/sn/ept/git/seminaire/cicd/resources/TagResourceTest.java
@@ -0,0 +1,38 @@
+package sn.ept.git.seminaire.cicd.resources;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TagResourceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ src/test/java/sn/ept/git/seminaire/cicd/services/TagServiceTest.java
@@ -0,0 +1,41 @@
+package sn.ept.git.seminaire.cicd.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TagServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteAll() {
+    }
+}
